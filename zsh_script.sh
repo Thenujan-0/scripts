c004efe9f1cf420e32943e060cf19b78dc518698
thenujan <sthenujan2002@gmail.com>
2021-10-27 07:24:48 +0530

    more imrpvemens

--- zsh_script.sh
+++ zsh_script.sh
@@ -10,10 +10,18 @@ chsh -s /bin/zsh $user
 mkdir ~/.scripts
 cd ~/.scripts
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
+echo "source ~/.scripts/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
 
 
 #zsh powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+
+#zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions
+echo "source ~/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh">> .zshrc
+
+#bind ` to autosuggest-accept
+echo "bindkey '`' autosuggest-accept" >> .zshrc
